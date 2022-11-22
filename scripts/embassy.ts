36fcddf989a9cf5f9564dc226db05d82366e1ea0
islandbitcoin <dread@start9.com>
2022-11-22 13:21:49 -0600

    removed js-main conversion

--- scripts/embassy.ts
+++ scripts/embassy.ts
@@ -2,4 +2,4 @@ export { setConfig } from "./procedures/setConfig.ts";
 export { properties } from "./procedures/properties.ts";
 export { getConfig } from "./procedures/getConfig.ts";
 export { migration } from "./procedures/migrations.ts";
-export { main } from "./procedures/main.ts";
+// export { main } from "./procedures/main.ts";
