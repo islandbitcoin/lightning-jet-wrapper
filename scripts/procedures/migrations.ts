b45c63e627665cac8c7f69caebcf1499d1bda9e6
Dread <34528298+islandbitcoin@users.noreply.github.com>
2024-06-26 17:43:47 -0400

    update deps

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -1,3 +1,3 @@
 import { compat, types as T } from "../deps.ts";
 
-export const migration: T.ExpectedExports.migration = compat.migrations.fromMapping({}, "1.6.0");
+export const migration: T.ExpectedExports.migration = compat.migrations.fromMapping({}, "1.6.0.1");
